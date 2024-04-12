e7f740337633497e0b42924fd7a9818923f5694f
arezooebrahimi <aeo.ebrahimi@gmial.com>
2024-04-12 11:04:55 +0330

    edit WeatherForecastController

--- Chapter_01/First Project/First Project/Controllers/WeatherForecastController.cs
+++ Chapter_01/First Project/First Project/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace First_Project.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
